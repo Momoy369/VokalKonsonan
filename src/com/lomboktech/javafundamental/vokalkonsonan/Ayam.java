7cf66b361f8cc1cf28abdd459623442a1e340cda
Momoy <irosyadi16@gmail.com>
2021-05-02 23:55:43 +0700

    Initial commit

---
+++ src/com/lomboktech/javafundamental/vokalkonsonan/Ayam.java
@@ -0,0 +1,13 @@
+package com.lomboktech.javafundamental.vokalkonsonan;
+
+public class Ayam {
+
+    public static void main(String[] args){
+        int jumlahAyam = 10;
+        if(jumlahAyam >= 10){
+            System.out.println("Wah, ayamnya banyak!");
+        } else{
+            System.out.println("Aduh, banyak ayam yang mati!");
+        }
+    }
+}
